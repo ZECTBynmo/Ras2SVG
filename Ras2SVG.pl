495079a769c62d45d8cc209bf41d14af67cdbf0f
unknown <ZECTBynmo@ZECTBynmo-PC.(none)>
2012-03-25 01:05:28 -0400

    Delete intermediate .eps files and add a few deug print statements

--- Ras2SVG.pl
+++ Ras2SVG.pl
@@ -3,13 +3,18 @@ use File::Basename;
 
 my @bmpFiles = glob("*.bmp");
 
+print( "\nConverting all .bmp files into .eps intermediate files\n" );
 for my $file (@bmpFiles) {
 	system("potrace.exe", $file);
 }
 
 my @epsFiles = glob("*.eps");
 
+print( "\nConverting all .eps files into .svg and deleting intermediates\n" );
 for my $file (@epsFiles) {
 	my ($filename) = substr($file, 0, rindex($file, '.'));
 	system( "gswin32.exe -dEPSFitPage -sDEVICE=svg -sOutputFile=$filename.svg '$file' quit.ps" );
+	unlink($file);
 }
+
+print( "\nJob Complete!\n" );
