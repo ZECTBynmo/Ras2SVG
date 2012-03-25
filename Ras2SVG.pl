923d3255803cbdcdaafa7cf10558bc8c90bd198a
unknown <ZECTBynmo@ZECTBynmo-PC.(none)>
2012-03-25 01:07:37 -0400

    Some comments

--- Ras2SVG.pl
+++ Ras2SVG.pl
@@ -1,6 +1,7 @@
 #!/bin/perl
 use File::Basename;
 
+## Grab all .bmp files
 my @bmpFiles = glob("*.bmp");
 
 print( "\nConverting all .bmp files into .eps intermediate files\n" );
@@ -8,12 +9,18 @@ for my $file (@bmpFiles) {
 	system("potrace.exe", $file);
 }
 
+## Grab all .eps files (presumably we just generated all of these)
 my @epsFiles = glob("*.eps");
 
 print( "\nConverting all .eps files into .svg and deleting intermediates\n" );
 for my $file (@epsFiles) {
+	## Grab the base filename of the file we're trying to convert
 	my ($filename) = substr($file, 0, rindex($file, '.'));
+	
+	## Convert the file to SVG
 	system( "gswin32.exe -dEPSFitPage -sDEVICE=svg -sOutputFile=$filename.svg '$file' quit.ps" );
+	
+	## Delete the file (note this will delete every .eps in the folder)
 	unlink($file);
 }
 
